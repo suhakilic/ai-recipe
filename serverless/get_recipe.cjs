f6dcd772b780f1b3a61de50d1dd40ad89af81034
Suhacan Kilic <kilicsuha@gmail.com>
2025-02-11 23:59:29 +0100

    hf updated

--- serverless/get_recipe.cjs
+++ serverless/get_recipe.cjs
@@ -1,7 +1,7 @@
 // serverless/get_recipe.js
 import fetch from "node-fetch";
 
-const HF_API_KEY = process.env.HF_API_KEY; // Ensure your API key is in environment variables
+const HF_API_KEY = new HfInference(process.env.HF_API_KEY);
 
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
--- src/ai.js
+++ src/ai.js
@@ -24,7 +24,7 @@ export async function getRecipeFromMistral(ingredientsArr) {
   //   console.error(err.message);
   // }
   try {
-    const response = await fetch("./.netlify/functions/get_recipe", {
+    const response = await fetch("./netlify/functions/get_recipe", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ ingredientsArr }), // Sending ingredients array
