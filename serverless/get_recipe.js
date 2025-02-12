8cd3162eb77e36350183f1dd7298188294f23d27
Suhacan Kilic <kilicsuha@gmail.com>
2025-02-12 01:07:44 +0100

    netlify update

--- serverless/get_recipe.js
+++ serverless/get_recipe.js
@@ -6,7 +6,7 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
 `;
 
-module.exports.handler= async (event, context) => {
+export async function handler(event, context)  {
   try {
     const { ingredientsArr } = JSON.parse(event.body);
     const ingredientsString = ingredientsArr.join(",");
