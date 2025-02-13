eaadde0a2d9ecd5c5f832a248ab1916b274bec7f
Suhacan Kilic <kilicsuha@gmail.com>
2025-02-13 15:12:44 +0100

    netlify update

--- serverless/get_recipe.js
+++ serverless/get_recipe.js
@@ -49,7 +49,7 @@ console.log("Hugging Face API Key: ", HF_API_KEY);  // Log the API Key to check 
 const hf = new HfInference(HF_API_KEY);
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. Keep it short. (Maks: 512 tokens)
 `;
 
 export async function handler(event, context) {
