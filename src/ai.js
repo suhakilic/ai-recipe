2dd437832256a59b7add9f24a24830119dd90588
Suhacan Kilic <kilicsuha@gmail.com>
2025-02-13 14:46:52 +0100

    netlify update

--- src/ai.js
+++ src/ai.js
@@ -1,3 +1,20 @@
+// // ai.js
+// export async function getRecipeFromMistral(ingredientsArr) {
+//   try {
+//     const response = await fetch("./.netlify/functions/get_recipe", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ ingredientsArr }),  // Sending ingredients array
+//     });
+
+//     const responseData = await response.json();
+//     return responseData.recipe;  // Return the recipe from the serverless function
+//   } catch (err) {
+//     console.error("Error fetching recipe:", err.message);
+//     return null;
+//   }
+// }
+
 // ai.js
 export async function getRecipeFromMistral(ingredientsArr) {
   try {
